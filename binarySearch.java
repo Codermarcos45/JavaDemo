30f0197a0a494665257ae5599796c811a1f4cf33
Codermarcos45 <biswarupsinghaofficial@gmail.com>
2023-04-13 09:32:24 +0530

    adding new files

---
+++ binarySearch.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class binarySearch {
+    
+    public static int binarySearchh(int arr[],int key) {
+        int sIdx = 0;
+        int eIdx = arr.length-1;
+
+        while(sIdx < eIdx) {
+            int mid = sIdx + (eIdx-sIdx);
+
+            if(arr[mid] == key) {
+                return mid;
+            } else if(arr[mid] < key) {
+                sIdx = mid + 1;
+            } else {
+                eIdx = mid - 1;
+            }
+
+        }
+
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {2,4,6,8,9};
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number that you want to find :> ");
+        int key = sc.nextInt();
+
+        System.out.println("The key "+key+" found at index : "+binarySearchh(arr,key));
+    }
+}
