b5a0db58261a69f10892630701ab33c00845c9a1
Codermarcos45 <biswarupsinghaofficial@gmail.com>
2023-04-21 18:17:07 +0530

    string complete

--- Practice.java
+++ Practice.java
@@ -3,23 +3,28 @@ import java.nio.charset.MalformedInputException;
 //remove duplicates : "apnacollege" >>> "apncoleg"
 
 class Practice  {
-    public static void removeDuplicates(String str) {
+    public static void firstUpperCase(String str) {
         StringBuilder sb = new StringBuilder("");
-        boolean alphabet[] = new boolean[26];
 
-        for(int i = 0;i<str.length();i++) {
+        sb.append(Character.toUpperCase(str.charAt(0)));
+        
+        for(int i=1;i<str.length();i++) {
 
-            if(alphabet[str.charAt(i) - 'a'] != true) {
-                alphabet[str.charAt(i) - 'a'] = true;
+            if(str.charAt(i) == ' ' && i < str.length()-1) {
                 sb.append(str.charAt(i));
-            } 
-        }
-        System.out.println(sb);
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            } else {
+                sb.append(str.charAt(i));
+            }
 
+        }
+
+        System.out.println(sb);
     }
     public static void main(String[] args) {
-        String str = "aaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbcsfsfewtfsd";
-        removeDuplicates(str);
+        String str = "hi, i am shradha";
+        firstUpperCase(str);
 
 
     }
